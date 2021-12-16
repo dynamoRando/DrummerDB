5546185ac458f43664529ad1a6a58bb3303a2e05
Randy Le <randyL04@hey.com>
2021-12-16 08:34:36 -0700

    change to record struct

--- src/DrummerDB.Core.Structures/TransactionEntryKey.cs
+++ src/DrummerDB.Core.Structures/TransactionEntryKey.cs
@@ -2,57 +2,10 @@
 
 namespace Drummersoft.DrummerDB.Core.Structures
 {
-    internal struct TransactionEntryKey : IEquatable<TransactionEntryKey>
+    internal record struct TransactionEntryKey
     {
         public Guid TransactionBatchId { get; set; }
         public int SequenceNumber { get; set; }
-
-        public bool Equals(TransactionEntryKey other)
-        {
-            // If parameter is null, return false.
-            if (Object.ReferenceEquals(other, null))
-            {
-                return false;
-            }
-
-            // Optimization for a common success case.
-            if (Object.ReferenceEquals(this, other))
-            {
-                return true;
-            }
-
-            // If run-time types are not exactly the same, return false.
-            if (this.GetType() != other.GetType())
-            {
-                return false;
-            }
-
-            return (this.TransactionBatchId == other.TransactionBatchId) &&
-                (this.SequenceNumber == other.SequenceNumber);
-        }
-
-        public static bool operator ==(TransactionEntryKey lhs, TransactionEntryKey rhs)
-        {
-            // Check for null on left side.
-            if (Object.ReferenceEquals(lhs, null))
-            {
-                if (Object.ReferenceEquals(rhs, null))
-                {
-                    // null == null = true.
-                    return true;
-                }
-
-                // Only the left side is null.
-                return false;
-            }
-            // Equals handles case of null on right side.
-            return lhs.Equals(rhs);
-        }
-
-        public static bool operator !=(TransactionEntryKey lhs, TransactionEntryKey rhs)
-        {
-            return !(lhs == rhs);
-        }
 
         public override string ToString()
         {
